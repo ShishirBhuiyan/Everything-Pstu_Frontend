2469607545cd88b488e27ad56196632bdfa9b311
Shishir Bhuiyan <60442502+ShishirBhuiyan@users.noreply.github.com>
2021-06-16 23:00:24 +0600

    Navbar

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+// Owl Carousel Start..................
+$(document).ready(function() {
+
+    $('.toggle').click(function(){
+       $('nav').toggleClass('active');
+    }); 
+});
+// Owl Carousel End..................
+
+
