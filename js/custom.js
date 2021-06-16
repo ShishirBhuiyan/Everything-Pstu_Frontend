b12a48adb6e50336e8b7c4cd0b5cf1ae094627f7
Shishir Bhuiyan <60442502+ShishirBhuiyan@users.noreply.github.com>
2021-06-17 01:33:17 +0600

    .

--- js/custom.js
+++ js/custom.js
@@ -9,14 +9,6 @@ $(document).ready(function() {
         $(".d-menu1").slideToggle(500);
         $(".check > i").toggleClass('rotate');
     });
-    
-/*     $(".d-btn2").click(function(){
-        $(".d-menu2").toggleClass('active');
-    });
-
-    $(".d-btn3").click(function(){
-        $(".d-menu3").toggleClass('active');
-    }); */
 
 });
 // Owl Carousel End..................
